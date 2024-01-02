20bba8b812e3151ed00751d3c8db78ac53fae791
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-02 17:38:41 +0700

    fix peta

--- app/jscroot/view/content/peta.js
+++ app/jscroot/view/content/peta.js
@@ -1,28 +1,30 @@
-// Set default center map
-var map = L.map("map").setView([-6.914744, 107.60981], 13);
+export function main() {
+  // Set default center map
+  var map = L.map("map").setView([-6.914744, 107.60981], 13);
 
-// Gunakan tile untuk layer dari openstreet
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+  // Gunakan tile untuk layer dari openstreet
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-// Get data
-const dataGeoJson = await fetch("../api/data.json").then((response) =>
-  response.json()
-);
+  // Get data
+  fetch("../api/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // set center ke data index ke 1
+      map.flyTo([
+        data.features[0].geometry.coordinates[1],
+        data.features[0].geometry.coordinates[0],
+      ]);
 
-// set center ke data index ke 1
-map.flyTo([
-  dataGeoJson.features[0].geometry.coordinates[1],
-  dataGeoJson.features[0].geometry.coordinates[0],
-]);
-
-// Load datageojsion ke map
-L.geoJSON(dataGeoJson).addTo(map, {
-  onEachFeature: function (feature, layer) {
-    // Bind a popup to the layer
-    layer.bindPopup(feature.properties.name);
-  },
-});
+      // Load datageojsion ke map
+      L.geoJSON(data).addTo(map, {
+        onEachFeature: function (feature, layer) {
+          // Bind a popup to the layer
+          layer.bindPopup(feature.properties.name);
+        },
+      });
+    });
+}
