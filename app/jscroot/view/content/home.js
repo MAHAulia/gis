e7d3ee5d59e6a39af552a06e3691e19f11a17acd
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-02 17:28:24 +0700

    add table

--- app/jscroot/view/content/home.js
+++ app/jscroot/view/content/home.js
@@ -13,7 +13,33 @@ export function main(){
             setInner("linestring",lineString.length.toLocaleString());
             setInner("poligon",polygon.length.toLocaleString());
             setInner("total",data.features.length.toLocaleString());
-            console.log(data, point, lineString, polygon)
+
+            const tbody = document.getElementById("table-body");
+
+            tbody.innerHTML = "";
+
+            data.features.forEach((rowData, key) => {
+                    const row = document.createElement("tr");
+
+                    const no = document.createElement("td");
+                    no.textContent = key + 1;
+                    row.appendChild(no);
+                
+                    const nama = document.createElement("td");
+                    nama.textContent = rowData.properties.name;
+                    row.appendChild(nama);
+                
+                    const type = document.createElement("td");
+                    type.textContent = rowData.geometry.type;
+                    row.appendChild(type);
+                
+                    const coord = document.createElement("td");
+                    coord.textContent = JSON.stringify(rowData.geometry);
+                    row.appendChild(coord);
+                
+                    tbody.appendChild(row);
+                }
+            );
         });
 }
 
