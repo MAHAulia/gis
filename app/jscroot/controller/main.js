3239d6292960de206648ab40c25d9041cb8eda82
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-02 16:52:05 +0700

    updaet path

--- app/jscroot/controller/main.js
+++ app/jscroot/controller/main.js
@@ -1,8 +1,8 @@
 import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.js";
-import { url } from "https://github.com/MAHAulia/gis/app/jscroot/url/config.js";
-import { getURLContentJS } from "https://github.com/MAHAulia/gis/app/jscroot/url/content.js";
-import { showMenu,activeLink } from "https://github.com/MAHAulia/gis/app/jscroot/controller/navbar.js";
-import { getContentURL } from "https://github.com/MAHAulia/gis/app/jscroot/url/content.js";
+import { url } from "https://mahaulia.github.io/gis/app//jscroot/url/config.js";
+import { getURLContentJS } from "https://mahaulia.github.io/gis/app//jscroot/url/content.js";
+import { showMenu,activeLink } from "https://mahaulia.github.io/gis/app//jscroot/controller/navbar.js";
+import { getContentURL } from "https://mahaulia.github.io/gis/app//jscroot/url/content.js";
 
 
 export function runAfterHashChange(evt){
--- app/jscroot/index.js
+++ app/jscroot/index.js
@@ -1,8 +1,8 @@
 import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.js";
-import { url } from "https://github.com/MAHAulia/gis/app/jscroot/url/config.js";
-import { getContentURL } from "https://github.com/MAHAulia/gis/app/jscroot/url/content.js";
+import { url } from "https://mahaulia.github.io/gis/app//jscroot/url/config.js";
+import { getContentURL } from "https://mahaulia.github.io/gis/app//jscroot/url/content.js";
 import {onHashChange} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.4/croot.js";
-import {runAfterHeader,runAfterContent,runAfterHashChange} from "https://github.com/MAHAulia/gis/app/jscroot/controller/main.js";
+import {runAfterHeader,runAfterContent,runAfterHashChange} from "https://mahaulia.github.io/gis/app//jscroot/controller/main.js";
 
 
 
