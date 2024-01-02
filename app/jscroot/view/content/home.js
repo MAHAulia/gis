6f666d16ec9c123e16816342463a1829cc0f2e13
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-02 17:12:32 +0700

    fix(getdata): get json data

--- app/jscroot/view/content/home.js
+++ app/jscroot/view/content/home.js
@@ -2,5 +2,11 @@ import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.2/croot.
 
 export function main(){
     setInner("biggreet","Halo Pengunjung");
+
+    fetch("../api/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    });
 }
 
