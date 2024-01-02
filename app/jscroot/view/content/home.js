f29aac39cc01027401a240db037ddc5bf9609020
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-02 17:15:28 +0700

    fix fetchurl

--- app/jscroot/view/content/home.js
+++ app/jscroot/view/content/home.js
@@ -3,10 +3,10 @@ import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.2/croot.
 export function main(){
     setInner("biggreet","Halo Pengunjung");
 
-    fetch("/api/data.json")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-    });
+    fetch("https://mahaulia.github.io/gis/app/api/data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        });
 }
 
